c9d812702452006926a24fd69eab9a9a072b4230
jerzha40 <jerryzhang40@gmail.com>
2025-03-04 12:08:37 +0800

    sokoban

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <SDL3/SDL_opengl.h>
 #include <glad/gl.h>
 #include "imgui.h"
-#include "imgui_impl_sdl.h"
+#include "imgui_impl_sdl3.h"
 #include "imgui_impl_opengl3.h"
 #include "game.h"
 #include <iostream>
